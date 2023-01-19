abbe46e11c145ca7ff5377f6bc15439730471bce
Martín Quesada <martinquesada@iremedy.com>
2023-01-19 15:17:07 -0800

    Customizing action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
-set -e
+#!/usr/bin/env bash
 
-export KUBECONFIG="${PWD}/kubeconfig"
-eksctl utils write-kubeconfig --cluster $INPUT_EKS_CLUSTER --kubeconfig $KUBECONFIG
-chmod 600 $KUBECONFIG
+# Login to Kubernetes Cluster.
+if [ -n "$CLUSTER_ROLE_ARN" ]; then
+  aws eks \
+    --region ${AWS_REGION} \
+    update-kubeconfig --name ${CLUSTER_NAME} \
+    --role-arn=${CLUSTER_ROLE_ARN}
+else
+  aws eks \
+    --region ${AWS_REGION} \
+    update-kubeconfig --name ${CLUSTER_NAME}
+fi
 
-if [[ -n "${INPUT_PLUGINS// /}" ]]
-then
-    plugins=$(echo $INPUT_PLUGINS | tr ",")
-
-    for plugin in $plugins
-    do
-        echo "installing helm plugin: [$plugin]"
-        helm plugin install $plugin
-    done
+if [ ! -z ${HELM_ECR_AWS_ACCOUNT_ID} ] && [ ! -z ${HELM_ECR_AWS_REGION} ]; then
+  echo "Login AWS ECR repository ${HELM_ECR_AWS_ACCOUNT_ID}.dkr.ecr.${HELM_ECR_AWS_REGION}.amazonaws.com"
+  aws ecr get-login-password \
+    --region ${HELM_ECR_AWS_REGION} | helm registry login \
+    --username AWS \
+    --password-stdin ${HELM_ECR_AWS_ACCOUNT_ID}.dkr.ecr.${HELM_ECR_AWS_REGION}.amazonaws.com
 fi
 
 echo "running entrypoint command(s)"
 
 response=$(sh -c "$INPUT_COMMAND")
 
-echo "::set-output name=response::$response"
+echo "response=$response" >> $GITHUB_ENV
