0b3005794219a154362b4a323f185246b6dfc0da
Martín Quesada <martinquesada@iremedy.com>
2023-01-19 20:50:33 -0800

    Customizing action

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ fi
 
 echo "running entrypoint command(s)"
 
-helm ${ARGUMENTS} -n${NAMESPACE} |awk '{print $9}' > helm-dev.txt
+helm ${ARGUMENTS} -n${NAMESPACE} |awk '{print $9}' > helm-${NAMESPACE}.txt
